d7d96fde9d9ca80c39075d4ebcf546f33c17896e
Sinan Suludağ <sinan.sldg23@gmail.com>
2025-04-18 23:02:02 +0300

    EfRepositoryBase olusturulup diger sınıflarda miras alinarak kullanildi.

---
+++ Core/DataAccess/EntityFramework/EfRepositoryBase.cs
@@ -0,0 +1,65 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfRepositoryBase<Tentity, TContext> : IEntityRepository<Tentity>
+        where Tentity : class, IEntity, new()
+        where TContext : DbContext, new()
+
+    {
+        public void Add(Tentity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public Tentity Get(Expression<Func<Tentity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<Tentity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Tentity> GetAll(Expression<Func<Tentity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? context.Set<Tentity>().ToList()
+                    : context.Set<Tentity>().Where(filter).ToList();
+            }
+        }
+
+        public void Remove(Tentity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Tentity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
